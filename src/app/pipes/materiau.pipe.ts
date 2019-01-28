93037dc18e5eb520a76fcf602b6c50abf8336885
Dimitri Batchevitch <dimitri.batchevitch@gmail.com>
2019-01-28 23:59:47 +0100

    boutons header blancs au demarrage

--- src/app/pipes/materiau.pipe.ts
+++ src/app/pipes/materiau.pipe.ts
@@ -9,12 +9,12 @@ export class MateriauPipe implements PipeTransform {
   transform(value: Test[], param_material?: string): Test[] {
     let result: Test[] = [];
 
-    if (param_material == "" || param_material == undefined) {
+    if (param_material == "" || param_material == undefined || param_material == null) {
       return value;
     } else {
         for (let i = 0; i < value.length; i++) {
           if (value[i].material != null) {
-            if (value[i].material.toLowerCase() == param_material.toLowerCase()) {
+            if (value[i].material.toLowerCase().indexOf(param_material.toLowerCase()) > -1) {
               result.push(value[i]);
             }
           } else {
